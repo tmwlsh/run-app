9c6f84fb6e350ff5ed45604360e199e5143f8484
Tom Walsh <tom@felltopdigital.com>
2025-04-17 12:32:11 +0100

    add

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,7 +2,7 @@ import Header from "../header";
 
 export default function Layout({children}) {
   return (
-    <div className="max-w-5xl mx-auto p-4 rounded-sm">
+    <div className="max-w-5xl mx-auto p-4 rounded-sm pb-20">
       <Header />
       <div className="max-w-2xl mx-auto">
         {children}
